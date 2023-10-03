37d6cd27131c975d0e7da16e9cfcd89b1f94945d
unknown <terrencemccarthysnkrs@gmail.com>
2023-10-03 00:44:25 -0500

    Set up file directory and required files

---
+++ Develop/Assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+
+    {
+        title: "How do you add 1 to a variable?",
+        choice: ["++", "--", "&&", "||"],
+        correctAnswer: "++",
+    },
+
+    {
+        title: "How do you subtract 1 from a variable?",
+        choice: ["++", "&&", "||", "--"],
+        correctAnswer: "--",
+    },
+
+    {
+        title: "How do you end a line of code?",
+        choice: ["}", ":", "]", ";"],
+        correctAnswer: ";",
+    },
+
+    {
+        title: "How do you break a line of code?",
+        choice: ["<next>", "<skip>", "<br>", "<other>"],
+        correctAnswer: "<br>",
+    },
+
+]
